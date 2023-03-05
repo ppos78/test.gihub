b0189b669307123a5882cc4814f901b309f0c3ed
drwill <ppos78@naver.com>
2023-03-05 14:29:59 +0900

    fourth commit

---
+++ 00_test.py
@@ -0,0 +1,12 @@
+print("안녕 닥터윌")
+# ctrl+shift ~ >>  terminal 생성
+# > python test.py 
+# > git init
+# > git add .
+# > git commit -m 'the first commit'
+
+print("세계 최초 AI 코딩 목사")
+# > git add .
+# > git commit -m 'the second commit'
+# > git remote add origin https://github.com/ppos78/test.gihub.git
+# > git push -u origin master
--- test.py
+++
@@ -1,8 +0,0 @@
-print("안녕 닥터윌")
-# ctrl+shift ~ >>  terminal 생성
-# > python test.py 
-# > git init
-# > git add .
-# > git commit -m 'first commit'
-
-print("세계 최초 AI 코딩 목사")
